67516665b582c72a43630b08af5cc98ca0f24e94
ictadmin <ictadm@jawakara.com>
2025-06-11 16:05:33 +0600

    [main.py] ParamCount system works!...
    - `sbntm_sh_2_hts_p_sbnt` -> `subnet_hosts_count`
    - `help` subcommand

--- main.py
+++ main.py
@@ -89,10 +89,33 @@ class IntegerOutofRangeException(Exception):
 
 VERSION: str = "0.0.1a"
 
-def subnet_mask_short2hosts_per_subnet(v: int) -> int:
+# Functions for subcommands
+def subnet_mask_short2hosts_per_subnet(inputs) -> int:
+    assert len(inputs) >= 1, "This should be handled at subcommand argument parsing!"
+
+    v = inputs.pop(0)
+    
+    v = v.removeprefix("/")
+
+    try:
+        v = int(v)
+    except Exception as e:
+        logger.error(f"{e}")
+        exit(1)
+
+    if v < 0 or v > 32:
+        logger.error("subnet mask should be between 0 ~ 32!")
+        exit(1)
+
     hosts_per_subnet: int = 2**(32 - v)-2
     logger.info(f"{hosts_per_subnet}")
     return hosts_per_subnet
+
+def hhelp(inputs):
+    assert len(inputs) >= 1, "This should be handled at subcommand argument parsing!"
+    program = inputs.pop(0)
+
+    usage(program)
 
 class Subcommand:
     def __init__(self, name: str, inputs: List[str], description: str, func, param_count: ParamCount):
@@ -108,7 +131,10 @@ class Subcommand:
     def __str__(self):
         return self.__repr__()
 
-subcommands = { "sbntm_sh_2_hts_p_sbnt": Subcommand("sbntm_sh_2_hts_p_sbnt", ["sbntm_sh"], "Calculates available hosts per subnet given short-hand subnet mask. eg: /24", subnet_mask_short2hosts_per_subnet, ParamCount(ParamCountType.EXACT, 1)) }
+subcommands = { "subnet_hosts_count": Subcommand("subnet_hosts_count", ["subnet_mask"], "Calculates available hosts per subnet given short-hand subnet mask. eg: /24", subnet_mask_short2hosts_per_subnet, ParamCount(ParamCountType.EXACT, 1)),
+               "test_atleast": Subcommand("test_atleast", ["arg1", "arg2"], "For testing the ParamCount.ATLEAST parsing.", None, ParamCount(ParamCountType.ATLEAST, 2)),
+               "help": Subcommand("help", [], "Help.", hhelp, ParamCount(ParamCountType.ATLEAST, 0)),
+               }
 
 def usage(program: str):
     print(f"{program} <subcommand>")
@@ -130,27 +156,39 @@ def main():
         if arg in subcommands:
             subcmd = subcommands[arg]
 
-            if subcmd.param_count._type == ParamCountType.ATLEAST:
-                assert False, "ParamCountType.ATLEAST"
-            elif subcmd.param_count._type == ParamCountType.EXACT:
-                parsed_inputs = []
+            parsed_inputs = []
+            if arg == "help":
+                parsed_inputs.append(program)
 
-                subcmd_input_count = len(sys.argv)
-                while subcmd.param_count.v > 0 and len(sys.argv) > 0:
-                    subcmd_input = sys.argv.pop(0)
-                    logger.info(f"{subcmd.inputs[len(subcmd.inputs)-subcmd.param_count.v]} = {subcmd_input}")
-                    subcmd.param_count.v -= 1
+            subcmd_input_count = len(sys.argv)
+            while subcmd.param_count.v > 0 and len(sys.argv) > 0:
+                subcmd_input = sys.argv.pop(0)
+                # logger.info(f"{subcmd.inputs[len(subcmd.inputs)-subcmd.param_count.v]} = {subcmd_input}")
+                parsed_inputs.append(subcmd_input)
+                subcmd.param_count.v -= 1
 
+
+            if subcmd.param_count._type == ParamCountType.EXACT:
                 if subcmd.param_count.v != len(sys.argv):
                     logger.error(f"Subcommand `{arg}` wanted {subcmd.param_count} argument(s), but got {subcmd_input_count} argument(s)!")
                     exit(1)
                 logger.info(f"param_count.v after parsing subcommand inputs: {subcmd.param_count.v}")
+            elif subcmd.param_count._type == ParamCountType.ATLEAST:
+                if subcmd.param_count.v != 0:
+                    logger.error(f"Subcommand `{arg}` wanted {subcmd.param_count} argument(s), but got {subcmd_input_count} argument(s)!")
+                    exit(1)
 
             elif subcmd.param_count._type == ParamCountType.NOMORE_THAN:
                 assert False, "ParamCountType.NOMORE_THAN"
             else:
                 logger.error("UNREACHABLE!")
                 exit(1)
+
+            # logger.info(f"Parsed subcommand inputs: {parsed_inputs}")
+            if subcmd.func:
+                subcmd.func(parsed_inputs)
+            else:
+                logger.warning(f"`{arg}` doesn't have a function attached to it!")
 
         else:
             logger.error(f"Unknown subcommand: `{arg}`")
