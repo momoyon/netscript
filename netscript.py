4a9d4478075eb05df6525c989e63d1dc20b063d6
ictadmin <ictadm@jawakara.com>
2025-10-20 10:56:11 +0600

    Implement subnet_count

--- netscript.py
+++ netscript.py
@@ -107,9 +107,50 @@ def subnet_mask_short2hosts_per_subnet(inputs) -> int:
         logger.error("subnet mask should be between 0 ~ 32!")
         exit(1)
 
-    hosts_per_subnet: int = 2**(32 - v)-2
+    hosts_per_subnet: int = 2**(32 - v) - 2
     logger.info(f"{hosts_per_subnet}")
     return hosts_per_subnet
+
+def subnet_count(inputs):
+    assert len(inputs) >= 1, "This should be handled at subcommand argument parsing!"
+    b = inputs.pop(0)
+    b = b.removeprefix("/")
+    try:
+        b = int(b)
+    except Exception as e:
+        logger.error(f"{e}")
+        exit(1)
+
+    if b < 0 or b > 32:
+        logger.error("base subnet mask should be between 0 ~ 32!")
+        exit(1)
+
+    v = inputs.pop(0)
+    v = v.removeprefix("/")
+    try:
+        v = int(v)
+    except Exception as e:
+        logger.error(f"{e}")
+        exit(1)
+
+    if v < 0 or v > 32:
+        logger.error("target subnet mask should be between 0 ~ 32!")
+        exit(1)
+
+    if v < b:
+        logger.info(f"0")
+        return 0
+    if v == 32:
+        logger.info(f"1")
+        return 1
+    if v == 31:
+        logger.info(f"2")
+        return 2
+
+    count = 2**(v - b)
+    logger.info(f"{count}")
+    return count
+
 
 def hhelp(inputs):
     assert len(inputs) >= 1, "This should be handled at subcommand argument parsing!"
@@ -153,6 +194,11 @@ def subnet_mask_short_to_long(inputs):
     logger.info(f"SUBNET_MASK_SHORT: /{subnet_mask_short}")
     logger.info(f"SUBNET_MASK_LONG:  {oct1}.{oct2}.{oct3}.{oct4} ({subnet_mask_long})")
 
+def subnet_mask_long_to_short(inputs):
+    assert len(inputs) >= 1, "This should be handled at subcommand argument parsing!"
+
+    long = inputs.pop(0)
+
 class Subcommand:
     def __init__(self, name: str, inputs: List[str], description: str, func, param_count: ParamCount):
         self.name = name
@@ -167,10 +213,13 @@ class Subcommand:
     def __str__(self):
         return self.__repr__()
 
-subcommands = { "subnet_hosts_count": Subcommand("subnet_hosts_count", ["subnet_mask"], "Calculates available hosts per subnet given short-hand subnet mask. eg: /24", subnet_mask_short2hosts_per_subnet, ParamCount(ParamCountType.EXACT, 1)),
-               "test_atleast": Subcommand("test_atleast", ["arg1", "arg2"], "For testing the ParamCount.ATLEAST parsing.", None, ParamCount(ParamCountType.ATLEAST, 2)),
-               "subnet_mask_short_to_long": Subcommand("subnet_mask_short_to_long", [ "subnet_mask_short" ], "Convert short form of a subnet mask to the long form.", subnet_mask_short_to_long, ParamCount(ParamCountType.EXACT, 1)), "help": Subcommand("help", [], "Help.", hhelp, ParamCount(ParamCountType.ATLEAST, 0)),
-               }
+subcommands = {
+    "subnet_hosts_count": Subcommand("subnet_hosts_count", ["target_subnet_mask"], "Calculates available hosts per subnet given short-hand subnet mask. eg: /24", subnet_mask_short2hosts_per_subnet, ParamCount(ParamCountType.EXACT, 1)),
+    "subnet_count": Subcommand("subnet_count", ["base_subnet_mask", "target_subnet_mask"], "Calculates number of subnets given short-hand base subnet mask and target subnet mask.", subnet_count, ParamCount(ParamCountType.EXACT, 2)),
+    "test_atleast": Subcommand("test_atleast", ["arg1", "arg2"], "For testing the ParamCount.ATLEAST parsing.", None, ParamCount(ParamCountType.ATLEAST, 2)),
+    "subnet_mask_short_to_long": Subcommand("subnet_mask_short_to_long", [ "subnet_mask_short" ], "Convert short form (CIDR) of a subnet mask to the long form.", subnet_mask_short_to_long, ParamCount(ParamCountType.EXACT, 1)), "help": Subcommand("help", [], "Help.", hhelp, ParamCount(ParamCountType.ATLEAST, 0)),
+    "subnet_mask_long_to_short": Subcommand("subnet_mask_long_to_short", [ "subnet_mask_long" ], "Convert long form of a subnet mask to the short form (CIDR).", subnet_mask_short_to_long, ParamCount(ParamCountType.EXACT, 1)), "help": Subcommand("help", [], "Help.", hhelp, ParamCount(ParamCountType.ATLEAST, 0)),
+}
 
 def usage(program: str):
     print(f"{program} <subcommand>")
@@ -206,12 +255,12 @@ def main():
 
             if subcmd.param_count._type == ParamCountType.EXACT:
                 if subcmd.param_count.v != len(sys.argv):
-                    logger.error(f"Subcommand `{arg}` wanted {subcmd.param_count} argument(s), but got {subcmd_input_count} argument(s)!")
+                    logger.error(f"Subcommand `{arg}` wanted {subcmd.param_count} argument(s), but got {subcmd_input_count} argument(s)!: WANTS {subcmd.inputs}")
                     exit(1)
                 # logger.info(f"param_count.v after parsing subcommand inputs: {subcmd.param_count.v}")
             elif subcmd.param_count._type == ParamCountType.ATLEAST:
                 if subcmd.param_count.v != 0:
-                    logger.error(f"Subcommand `{arg}` wanted {subcmd.param_count} argument(s), but got {subcmd_input_count} argument(s)!")
+                    logger.error(f"Subcommand `{arg}` wanted {subcmd.param_count} argument(s), but got {subcmd_input_count} argument(s)!: WANTS {subcmd.inputs}")
                     exit(1)
 
             elif subcmd.param_count._type == ParamCountType.NOMORE_THAN:
